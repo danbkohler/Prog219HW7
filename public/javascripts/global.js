ca379145a26ba56a73a54e1f789b3cc6b4d25014
danbkohler <danbkohler@gmail.com>
2017-06-07 13:35:46 -0700

    Update global.js
    Cleaning up code

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,26 +1,20 @@
-// Userlist data array for filling in info box
+// Userlist data array
 var userListData = [];
 
-// DOM Ready =============================================================
 $(document).ready(function() {
 
-    // Populate the user table on initial page load
+    // Populate user table
     populateTable();
 
-    // Username link click
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
-    // Add User button click
     $('#btnAddUser').on('click', addUser);
 
-    // Delete User link click
     $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 
 });
 
-// Functions =============================================================
-
-// Fill table with data
+// Fill table
 function populateTable() {
 
     // Empty content string
@@ -29,10 +23,8 @@ function populateTable() {
     // jQuery AJAX call for JSON
     $.getJSON( '/users/userlist', function( data ) {
 
-        // Stick our user data array into a userlist variable in the global object
         userListData = data;
 
-        // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
             tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
@@ -41,7 +33,6 @@ function populateTable() {
             tableContent += '</tr>';
         });
 
-        // Inject the whole content string into our existing HTML table
         $('#userList table tbody').html(tableContent);
     });
 };
@@ -49,19 +40,14 @@ function populateTable() {
 // Show User Info
 function showUserInfo(event) {
 
-    // Prevent Link from Firing
     event.preventDefault();
 
-    // Retrieve username from link rel attribute
     var thisUserName = $(this).attr('rel');
 
-    // Get Index of object based on id value
     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
 
-    // Get our User Object
     var thisUserObject = userListData[arrayPosition];
 
-    //Populate Info Box
     $('#userInfoName').text(thisUserObject.fullname);
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
@@ -73,16 +59,13 @@ function showUserInfo(event) {
 function addUser(event) {
     event.preventDefault();
 
-    // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
     $('#addUser input').each(function(index, val) {
         if($(this).val() === '') { errorCount++; }
     });
 
-    // Check and make sure errorCount's still at zero
     if(errorCount === 0) {
 
-        // If it is, compile all user info into one object
         var newUser = {
             'username': $('#addUser fieldset input#inputUserName').val(),
             'email': $('#addUser fieldset input#inputUserEmail').val(),
@@ -92,7 +75,6 @@ function addUser(event) {
             'gender': $('#addUser fieldset input#inputUserGender').val()
         }
 
-        // Use AJAX to post the object to our adduser service
         $.ajax({
             type: 'POST',
             data: newUser,
@@ -100,26 +82,19 @@ function addUser(event) {
             dataType: 'JSON'
         }).done(function( response ) {
 
-            // Check for successful (blank) response
             if (response.msg === '') {
 
-                // Clear the form inputs
                 $('#addUser fieldset input').val('');
 
-                // Update the table
                 populateTable();
 
             }
             else {
-
-                // If something goes wrong, alert the error message that our service returned
                 alert('Error: ' + response.msg);
-
             }
         });
     }
     else {
-        // If errorCount is more than 0, error out
         alert('Please fill in all fields');
         return false;
     }
@@ -130,13 +105,11 @@ function deleteUser(event) {
 
     event.preventDefault();
 
-    // Pop up a confirmation dialog
     var confirmation = confirm('Are you sure you want to delete this user?');
 
-    // Check and make sure the user confirmed
     if (confirmation === true) {
 
-        // If they did, do our delete
+        // If true, delete
         $.ajax({
             type: 'DELETE',
             url: '/users/deleteuser/' + $(this).attr('rel')
@@ -149,17 +122,13 @@ function deleteUser(event) {
                 alert('Error: ' + response.msg);
             }
 
-            // Update the table
             populateTable();
 
         });
 
     }
     else {
-
-        // If they said no to the confirm, do nothing
         return false;
-
     }
 
 };
